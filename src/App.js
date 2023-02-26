39a2f1ae0b16db577c5aba487d8b2c26ff440587
Fawaz441 <abdulsalamfawaz4@gmail.com>
2023-02-26 22:07:02 +0100

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppRoutes from './Routes'
 
 const App = () => (
   <div className='min-h-screen bg-dark'>
     <div>
-      <BrowserRouter basename="https://aboosalman.github.io/">
+      <HashRouter>
         <AppRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   </div>
 )
